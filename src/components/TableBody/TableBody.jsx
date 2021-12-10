2abe6402bafa90d00b0a2469ae9c7591d9483dbe
Vimal Raveendran <vimalraveendra@gmail.com>
2021-12-10 12:43:06 +0100

    added mediqueris for small screens , also updated the TableBody component to render on small screens & also written Readme file

--- src/components/TableBody/TableBody.jsx
+++ src/components/TableBody/TableBody.jsx
@@ -1,19 +1,20 @@
 const TableBody = ({id,description,date,assigned,status,priority,item})=>{  
-      
-        return(
-            <tr>
-              <td >{id}</td>
-              <td >{description}</td>
-              <td>{date}</td>
-            
-                <td>{assigned.length>0? assigned[0].person_name:""}</td>
-                <td >{assigned.length>0? assigned[0].status:""}</td> 
-              
-              <td>{status}</td>
-              <td>{priority}</td>
-    
-            </tr>
-        )
+    let arr =Object.keys(item).map(item=>(item==="work_order_id")?item =(item[0]+item[1]+" "+item.slice(11)).toUpperCase(): item.toUpperCase())
+        
+    return(
+        <tr>
+          <td data-label={arr[0]}>{id}</td>
+          <td data-label={arr[1]}>{description}</td>
+          <td data-label={arr[2]}>{date}</td>
+        
+            <td data-label={'PERSON NAME'}>{assigned.length>0? assigned[0].person_name:""}</td>
+            <td data-label={arr[3]}>{assigned.length>0? assigned[0].status:""}</td> 
+          
+          <td data-label={arr[4]}>{status}</td>
+          <td data-label={arr[5]}>{priority}</td>
+
+        </tr>
+       )
     }
     
     export default TableBody;
